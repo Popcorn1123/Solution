3ef975071da698dc8c8653a1f3d47789f05485dd
Popcorn1123 <nuofeidai@gmail.com>
2023-02-03 13:12:44 +0800

    不开long long 见祖宗

---
+++ P1678.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int m, n, stu[100001], sch[100001], ans = 0, mid, l, r;
+signed main() {
+	cin >> m >> n; // m表示学校数 n表示学生数
+	for (int i = 1;i <= m;++i) {
+		scanf("%d", &sch[i]);
+	}
+	for (int i = 1;i <= n;++i) {
+		scanf("%d", &stu[i]);
+	}
+	sort(sch + 1, sch + m + 1);
+	for (int i = 1;i <= n;++i) { // 二分法分配sch[]
+		l = 1;r = m;
+		while (l < r) {
+			mid = (l + r) >> 1;
+			if (sch[mid] > stu[i]) {
+				r = mid;
+			} else {
+				l = mid + 1;
+			}
+		}
+		if (stu[i] < sch[1]) {
+			ans += sch[1] - stu[i];
+		} else {
+			ans += min(abs(sch[l] - stu[i]), abs(sch[l - 1] - stu[i]));
+		}
+	}
+	cout << ans;
+}
