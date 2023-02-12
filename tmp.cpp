88a71b804d97b7991fef88c601a0ad91fa4421cd
Popcorn1123 <nuofeidai@gmail.com>
2023-02-12 20:32:04 +0800

    0212

--- tmp.cpp
+++ tmp.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+char test500[500];
 int main() {
-	printf("\"\"");
-	printf("\\\\\n");
+	memset(test500,'0',sizeof(test500));
+	cin>>(test500+1);
+	for(int i=0;i<=499;++i)cout<<test500[i];
 	return 0;
 }
